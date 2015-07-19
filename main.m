06b2d2bcf6fb07b460b7048ad100d150568aa5e9
Wojciech A. Koszek <wojciech@koszek.com>
2015-07-18 20:00:05 -0700

    Add the simple unit test.

--- main.m
+++ main.m
@@ -1,5 +1,23 @@
+#import <Foundation/Foundation.h>
+#import "PlayingCardDeck.h"
+
+#include <assert.h>
+
 int
 main()
 {
+	@autoreleasepool {
+		PlayingCardDeck *deck = [PlayingCardDeck new];
+		Card	*card;
+		int	card_cnt = 0;
 
+		do {
+			card = [deck drawRandomCard];
+			if (card) {
+				card_cnt++;
+			}
+		} while (card != nil);
+		assert(card_cnt == 52 && "something is wrong with card count");
+	}
+	return 0;
 }
