5260a813acb0da14c1511b2cdc3b4ab8dc2af9c6
Wojciech A. Koszek <wojciech@koszek.com>
2015-07-18 19:40:31 -0700

    Number from 1, not 0.

--- PlayingCardDeck.m
+++ PlayingCardDeck.m
@@ -9,7 +9,7 @@
 		return self;
 	}
 	for (NSString *suit in [PlayingCard validSuits]) {
-		for (NSUInteger rank = 0;
+		for (NSUInteger rank = 1;
 					rank < [PlayingCard maxRank]; rank++) {
 			PlayingCard *card = [[PlayingCard alloc] init];
 			card.rank = rank;
