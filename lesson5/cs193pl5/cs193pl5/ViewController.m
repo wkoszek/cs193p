7241ee53c9304cfd1b628c102353442b971a8885
Wojciech A. Koszek <wojciech@koszek.com>
2015-12-22 01:15:28 -0800

    Add notification center.
    Should work when the user goes and enables different system-wide
    fonts.
    
    As of right now it doesn't work and I'm not sure why. I got one
    weird warnings about:
    
    bogus value for UIPreferredContentSizeCategoryName
    
    Will have to investigate.

--- lesson5/cs193pl5/cs193pl5/ViewController.m
+++ lesson5/cs193pl5/cs193pl5/ViewController.m
@@ -12,6 +12,7 @@
 @property (weak, nonatomic) IBOutlet UITextView *body;
 @property (weak, nonatomic) IBOutlet UILabel *headline;
 @property (weak, nonatomic) IBOutlet UIButton *changeBody;
+@property (weak, nonatomic) IBOutlet UIButton *outlineButton;
 
 @end
 
@@ -20,7 +21,53 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    NSMutableAttributedString *title =
+    [[NSMutableAttributedString alloc] initWithString:self.outlineButton.currentTitle];
+
+    [title setAttributes:@{
+                           NSStrokeWidthAttributeName : @3,
+                           NSStrokeColorAttributeName : self.outlineButton.tintColor
+                          }
+                   range:NSMakeRange(0, [title length])];
+    [self.outlineButton setAttributedTitle:title forState:UIControlStateNormal];
 }
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    NSLog(@"viewWillAppear");
+    [super viewWillDisappear:animated];
+
+    [self usePreferredFonts];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(preferredFontsChanged:)
+                                                 name:UIContentSizeCategoryDidChangeNotification
+                                               object:nil];
+}
+
+- (void) viewWillDisappear:(BOOL)animated
+{
+    NSLog(@"viewWillDisappear");
+    [super viewWillDisappear:animated];
+
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIContentSizeCategoryDidChangeNotification
+                                                  object:nil];
+}
+
+- (void)preferredFontsChanged:(NSNotification *)notification
+{
+    NSLog(@"Preferred fonts had changed");
+    [self usePreferredFonts];
+}
+
+- (void) usePreferredFonts
+{
+    NSLog(@"use the right fonts!");
+    self.body.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    self.headline.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
