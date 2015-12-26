44655eedffb9b54e37332b8f8a6b142571ac10a6
Wojciech A. Koszek <wojciech@koszek.com>
2015-12-26 13:09:22 -0800

    Display the card's rank/suit.
    Didn't want to spend time on analyzing why Paul's ratios for sizes
    are set the way they are, so in my case I'm hardcoding some sizes
    for round rectangles etc.
    
    When I run it on MBA 13" with iPhone 4s scaled to 50% it looks OK.

--- lesson7/SuperCard/SuperCard/PlayingCardView.m
+++ lesson7/SuperCard/SuperCard/PlayingCardView.m
@@ -10,6 +10,8 @@
 
 @implementation PlayingCardView
 
+#pragma mark - Public API
+
 - (void) setSuit:(NSString *)suit
 {
     _suit = suit;
@@ -29,12 +31,63 @@
     [self setNeedsDisplay];
 }
 
-/*
+#pragma mark - Drawing
+
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (void)drawRect:(CGRect)rect
+{
+    UIBezierPath *roundedRect = [UIBezierPath bezierPathWithRoundedRect:self.bounds cornerRadius:12];
+
+    [roundedRect addClip];
+
+    [[UIColor whiteColor] setFill];
+    UIRectFill(self.bounds);
+
+    [[UIColor blackColor] setStroke];
+    [roundedRect stroke];
+
+    [self drawCorners];
 }
-*/
+
+- (NSString *) rankAsString
+{
+	return @[@"?", @"A", @"2",
+		@"3", @"4", @"5",
+		@"6", @"7", @"8",
+		@"9", @"10", @"J",
+             @"Q", @"K" ][self.rank];
+}
+
+- (void) drawCorners
+{
+    NSMutableParagraphStyle *paragraphStyle = [NSMutableParagraphStyle new];
+
+    paragraphStyle.alignment = NSTextAlignmentCenter;
+    UIFont *cornerFont = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    cornerFont = [cornerFont fontWithSize:cornerFont.pointSize * 5];
+
+    NSAttributedString *cornerText = [[NSAttributedString alloc]
+        initWithString:[NSString stringWithFormat:@"%@\n%@", [self rankAsString], self.suit]
+            attributes:@{NSFontAttributeName: cornerFont, NSParagraphStyleAttributeName: paragraphStyle}];
+
+    CGRect textBounds;
+    textBounds.origin = CGPointMake(10, 10);
+    textBounds.size = [cornerText size];
+
+    [cornerText drawInRect:textBounds];
+}
+
+#pragma mark - Initialization
+- (void) setup {
+    self.backgroundColor = nil;
+    self.opaque = NO;
+    self.contentMode = UIViewContentModeRedraw;
+}
+
+- (void) awakeFromNib {
+    NSLog(@"awakeFromNib");
+    [self setup];
+}
 
 @end
