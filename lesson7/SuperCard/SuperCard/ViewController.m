78ef449d523b320f7aa265ce4b121ab42b59c360
Wojciech A. Koszek <wojciech@koszek.com>
2015-12-26 16:09:21 -0800

    Change to the proper heart symbol.

--- lesson7/SuperCard/SuperCard/ViewController.m
+++ lesson7/SuperCard/SuperCard/ViewController.m
@@ -19,7 +19,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.playingCardView.suit = @"♥︎";
+    self.playingCardView.suit = @"♥";
     self.playingCardView.rank = 13;
 }
 
