2951428bf48acb5a373d0657f6e8f0905c7ea56b
Wojciech A. Koszek <wojciech@koszek.com>
2015-12-29 17:17:55 -0800

    Add an empty project for lesson 10.

---
+++ lesson10/lesson10/ImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImageViewController.h
+//  lesson10
+//
+//  Created by Wojciech Koszek (home) on 12/29/15.
+//  Copyright © 2015 Wojciech Koszek (home). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageViewController : UIViewController
+
+@end
---
+++ lesson10/lesson10/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  lesson10
+//
+//  Created by Wojciech Koszek (home) on 12/29/15.
+//  Copyright © 2015 Wojciech Koszek (home). All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
