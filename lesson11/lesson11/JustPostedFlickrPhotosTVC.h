785b2b1c67bb79463de318afb496f09cc0a14a8a
Wojciech A. Koszek <wojciech@koszek.com>
2015-12-30 16:45:08 -0800

    Bring a child class for just posted photos.

---
+++ lesson11/lesson11/JustPostedFlickrPhotosTVC.h
@@ -0,0 +1,13 @@
+//
+//  JustPostedFlickrPhotosTVC.h
+//  lesson11
+//
+//  Created by Wojciech Koszek (home) on 12/30/15.
+//  Copyright © 2015 Wojciech Koszek (home). All rights reserved.
+//
+
+#import "FlickrPhotoTVC.h"
+
+@interface JustPostedFlickrPhotosTVC : FlickrPhotoTVC
+
+@end
