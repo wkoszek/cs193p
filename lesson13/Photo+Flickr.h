e0bfaba76295c829f49dacfa2c78416584cc2589
Wojciech A. Koszek <wojciech@koszek.com>
2016-01-03 01:59:41 -0800

    Add Flickr category.

---
+++ lesson13/Photo+Flickr.h
@@ -0,0 +1,13 @@
+//
+//  Photo+Flickr.h
+//  lesson13
+//
+//  Created by Wojciech Koszek (home) on 1/3/16.
+//  Copyright © 2016 Wojciech Koszek (home). All rights reserved.
+//
+
+#import "Photo.h"
+
+@interface Photo (Flickr)
+
+@end
---
+++ lesson13/Photo+Flickr.m
@@ -0,0 +1,13 @@
+//
+//  Photo+Flickr.m
+//  lesson13
+//
+//  Created by Wojciech Koszek (home) on 1/3/16.
+//  Copyright © 2016 Wojciech Koszek (home). All rights reserved.
+//
+
+#import "Photo+Flickr.h"
+
+@implementation Photo (Flickr)
+
+@end
