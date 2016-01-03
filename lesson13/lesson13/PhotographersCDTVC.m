4545f366ced047d1f53edf85d30535d607599b81
Wojciech A. Koszek <wojciech@koszek.com>
2016-01-03 15:51:32 -0800

    Add new class for Photographers management.

---
+++ lesson13/lesson13/PhotographersCDTVC.m
@@ -0,0 +1,13 @@
+//
+//  PhotographersCDTVC.m
+//  lesson13
+//
+//  Created by Wojciech Koszek (home) on 1/3/16.
+//  Copyright © 2016 Wojciech Koszek (home). All rights reserved.
+//
+
+#import "PhotographersCDTVC.h"
+
+@implementation PhotographersCDTVC
+
+@end
