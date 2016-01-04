8e008b165e17617efff65af93e3126f723cd1b05
Wojciech A. Koszek <wojciech@koszek.com>
2016-01-03 17:07:56 -0800

    Add fetching from Flickr (URLSession stuff)

--- lesson13/lesson13/AppDelegate.m
+++ lesson13/lesson13/AppDelegate.m
@@ -8,9 +8,10 @@
 
 #import "AppDelegate.h"
 #import "PhotomaniaAppDelegate+MOC.h"
+#import "FlickrFetcher.h"
+#import "Photo+Flickr.h"
 
-
-@interface AppDelegate ()
+@interface AppDelegate () <NSURLSessionDownloadDelegate>
 @property (copy, nonatomic) void (^flickrDownloadBackgroundURLSessionCompletionHandler)();
 @property (strong, nonatomic) NSURLSession *flickrDownloadSession;
 @property (strong, nonatomic) NSTimer *flickrForegroundTimer;
@@ -28,7 +29,80 @@
 
     self.photoDatabaseContext = [self createMainQueueManagedObjectContext];
 
+    [self startFlickrFetch];
     return YES;
+}
+
+- (void) startFlickrFetch
+{
+    [self.flickrDownloadSession getTasksWithCompletionHandler:^(NSArray<NSURLSessionDataTask *> * _Nonnull dataTasks, NSArray<NSURLSessionUploadTask *> * _Nonnull uploadTasks, NSArray<NSURLSessionDownloadTask *> * _Nonnull downloadTasks) {
+        if (![downloadTasks count]) {
+            NSURLSessionDownloadTask *task = [self.flickrDownloadSession downloadTaskWithURL:[FlickrFetcher URLforRecentGeoreferencedPhotos]];
+            task.taskDescription = FLICKR_FETCH;
+            [task resume];
+        } else {
+            for (NSURLSessionDownloadTask *task in downloadTasks) {
+                [task resume];
+            }
+        }
+    }];
+}
+
+- (NSURLSession *)flickrDownloadSession
+{
+    if (!_flickrDownloadSession) {
+        static dispatch_once_t onceToken;
+
+        dispatch_once(&onceToken, ^{
+            NSURLSessionConfiguration *config = [NSURLSessionConfiguration backgroundSessionConfigurationWithIdentifier:FLICKR_FETCH];
+            config.allowsCellularAccess = NO;
+            _flickrDownloadSession = [NSURLSession sessionWithConfiguration:config
+                                                                   delegate:self
+                                                              delegateQueue:nil];
+        });
+    }
+    return _flickrDownloadSession;
+}
+
+-(void)URLSession:(NSURLSession *)session
+     downloadTask:(NSURLSessionDownloadTask *)downloadTask
+didFinishDownloadingToURL:(NSURL *)localFile
+{
+    if ([downloadTask.taskDescription isEqualToString:FLICKR_FETCH]) {
+        NSManagedObjectContext *context = self.photoDatabaseContext;
+        if (context) {
+            NSArray *photos = [self flickrPhotosAtURL:localFile];
+            [context performBlock:^{
+                [Photo loadPhotosFromFlickrArray:photos
+                        intoManagedObjectContext:context];
+                [context save:NULL];
+            }];
+        } else {
+            [self flickrDownloadTasksMightBeComplete];
+        }
+    }
+}
+
+-(void)URLSession:(NSURLSession *)session
+             downloadTask:(NSURLSessionDownloadTask *)downloadTask
+             didResumeAtOffset:(int64_t)fileOffset
+             expectedTotalBytes:(int64_t)expectedTotalBytes
+{
+
+}
+
+- (void)URLSession:(NSURLSession *)session
+      downloadTask:(NSURLSessionDownloadTask *)downloadTask
+      didWriteData:(int64_t)bytesWritten
+ totalBytesWritten:(int64_t)totalBytesWritten
+totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite
+{
+
+}
+
+- (void)flickrDownloadTasksMightBeComplete
+{
+
 }
 
 @end
