dcc87e1f7895087f64cf17bab340697079271f5f
Wojciech A. Koszek <wojciech@koszek.com>
2016-01-03 17:20:50 -0800

    Add the notification message

---
+++ lesson13/PhotoDatabaseAvailability.h
@@ -0,0 +1,15 @@
+//
+//  PhotoDatabaseAvailability.h
+//  lesson13
+//
+//  Created by Wojciech Koszek (home) on 1/3/16.
+//  Copyright © 2016 Wojciech Koszek (home). All rights reserved.
+//
+
+#ifndef PhotoDatabaseAvailability_h
+#define PhotoDatabaseAvailability_h
+
+#define PhotoDatabaseAvailabilityNotification @"PhotoDatabaseNotification"
+#define PhotoDatabaseAvailabilityContext @"Context"
+
+#endif /* PhotoDatabaseAvailability_h */
