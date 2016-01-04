dfc35f92eada79ab8cc8464804529c458eaefdca
Wojciech A. Koszek <wojciech@koszek.com>
2016-01-03 17:34:33 -0800

    Add missing helper routines.

--- lesson13/lesson13/AppDelegate.m
+++ lesson13/lesson13/AppDelegate.m
@@ -101,10 +101,6 @@ totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite
 
 }
 
-- (void)flickrDownloadTasksMightBeComplete
-{
-
-}
 
 - (void)setPhotoDatabaseContext:(NSManagedObjectContext *)photoDatabaseContext
 {
@@ -118,5 +114,43 @@ totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite
                                                         object:self
                                                       userInfo:userInfo];
 }
+
+
+// ---------------- ORIGINAL HELPER ROUTINES ---------------
+- (void)flickrDownloadTasksMightBeComplete
+{
+    if (self.flickrDownloadBackgroundURLSessionCompletionHandler) {
+        [self.flickrDownloadSession getTasksWithCompletionHandler:^(NSArray *dataTasks, NSArray *uploadTasks, NSArray *downloadTasks) {
+            // we're doing this check for other downloads just to be theoretically "correct"
+            //  but we don't actually need it (since we only ever fire off one download task at a time)
+            // in addition, note that getTasksWithCompletionHandler: is ASYNCHRONOUS
+            //  so we must check again when the block executes if the handler is still not nil
+            //  (another thread might have sent it already in a multiple-tasks-at-once implementation)
+            if (![downloadTasks count]) {  // any more Flickr downloads left?
+                // nope, then invoke flickrDownloadBackgroundURLSessionCompletionHandler (if it's still not nil)
+                void (^completionHandler)() = self.flickrDownloadBackgroundURLSessionCompletionHandler;
+                self.flickrDownloadBackgroundURLSessionCompletionHandler = nil;
+                if (completionHandler) {
+                    completionHandler();
+                }
+            } // else other downloads going, so let them call this method when they finish
+        }];
+    }
+}
+
+// standard "get photo information from Flickr URL" code
+
+- (NSArray *)flickrPhotosAtURL:(NSURL *)url
+{
+    NSDictionary *flickrPropertyList;
+    NSData *flickrJSONData = [NSData dataWithContentsOfURL:url];  // will block if url is not local!
+    if (flickrJSONData) {
+        flickrPropertyList = [NSJSONSerialization JSONObjectWithData:flickrJSONData
+                                                             options:0
+                                                               error:NULL];
+    }
+    return [flickrPropertyList valueForKeyPath:FLICKR_RESULTS_PHOTOS];
+}
+
 
 @end
