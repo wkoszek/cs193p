414f6e1f40b8c651419c1db4f21fdf7033ef4a13
Wojciech A. Koszek <wojciech@koszek.com>
2016-01-07 09:19:37 -0800

    Wire to Core Data.

--- lesson13/PhotosCDTVC.m
+++ lesson13/PhotosCDTVC.m
@@ -7,31 +7,18 @@
 //
 
 #import "PhotosCDTVC.h"
-
-@interface PhotosCDTVC ()
-
-@end
+#import "Photo.h"
 
 @implementation PhotosCDTVC
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
-}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"Photo Cell"];
+    Photo *photo = [self.fetchedResultsController objectAtIndexPath:indexPath];
+    cell.textLabel.text = photo.title;
+    cell.detailTextLabel.text = photo.subtitle;
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    return cell;
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
