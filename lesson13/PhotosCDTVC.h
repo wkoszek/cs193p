d7a1a3b7a8b5ff9c90330624e304893fb6859a68
Wojciech A. Koszek <wojciech@koszek.com>
2016-01-07 09:11:20 -0800

    Add Photos table view controller.

---
+++ lesson13/PhotosCDTVC.h
@@ -0,0 +1,15 @@
+//
+//  PhotosCDTVC.h
+//  lesson13
+//
+//  Created by Wojciech Koszek (home) on 1/7/16.
+//  Copyright © 2016 Wojciech Koszek (home). All rights reserved.
+//
+
+#import "CoreDataTableViewController.h"
+
+@interface PhotosCDTVC : CoreDataTableViewController
+
+// hook up fetchedResultsController to any Photo fetch request
+
+@end
---
+++ lesson13/PhotosCDTVC.m
@@ -0,0 +1,37 @@
+//
+//  PhotosCDTVC.m
+//  lesson13
+//
+//  Created by Wojciech Koszek (home) on 1/7/16.
+//  Copyright © 2016 Wojciech Koszek (home). All rights reserved.
+//
+
+#import "PhotosCDTVC.h"
+
+@interface PhotosCDTVC ()
+
+@end
+
+@implementation PhotosCDTVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
