40b1d37f5fb795b805f96e8dcab3907aaac00cda
Wojciech A. Koszek <wojciech@koszek.com>
2016-01-06 20:53:32 -0800

    Add a class required by the storyboard.

---
+++ lesson13/PhotosByPhotographerCDTVCViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotosByPhotographerCDTVCViewController.h
+//  lesson13
+//
+//  Created by Wojciech Koszek (home) on 1/6/16.
+//  Copyright © 2016 Wojciech Koszek (home). All rights reserved.
+//
+
+#import "CoreDataTableViewController.h"
+
+@interface PhotosByPhotographerCDTVCViewController : CoreDataTableViewController
+
+@end
