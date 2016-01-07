e1d3b9991e82d645bb3ecf86cd2580832d7c8b89
Wojciech A. Koszek <wojciech@koszek.com>
2016-01-06 20:54:10 -0800

    Add an ImageViewController class from lesson 11

---
+++ lesson13/ImageViewController.m
@@ -0,0 +1,134 @@
+//
+//  ImageViewController.m
+//  lesson10
+//
+//  Created by Wojciech Koszek (home) on 12/29/15.
+//  Copyright © 2015 Wojciech Koszek (home). All rights reserved.
+//
+
+#import "ImageViewController.h"
+
+@interface ImageViewController () <UIScrollViewDelegate, UISplitViewControllerDelegate>
+@property (nonatomic, strong) UIImageView *imageView;
+@property (nonatomic, strong) UIImage *image;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
+@end
+
+@implementation ImageViewController
+
+- (void) setScrollView:(UIScrollView *)scrollView
+{
+    _scrollView = scrollView;
+    _scrollView.minimumZoomScale= 0.2;
+    _scrollView.maximumZoomScale = 2.0;
+    _scrollView.delegate = self;
+    self.scrollView.contentSize = self.image ? self.image.size : CGSizeZero;
+}
+
+-(UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return self.imageView;
+}
+
+- (void)setImageURL:(NSURL *)imageURL
+{
+    _imageURL = imageURL;
+//    self.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:self.imageURL]];
+    [self startDownloadingImage];
+}
+
+- (void)startDownloadingImage
+{
+    self.image = nil;
+    if (self.imageURL) {
+        [self.spinner startAnimating];
+        NSURLRequest *request = [NSURLRequest requestWithURL:self.imageURL];
+        NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration ephemeralSessionConfiguration];
+        NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+        NSURLSessionDownloadTask *task = [session downloadTaskWithRequest:request completionHandler:^(NSURL * _Nullable localFile, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+            if (!error) {
+                if ([request.URL isEqual:self.imageURL]) {
+                    UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:localFile]];
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        self.image = image;
+                    });
+                }
+            }
+        }];
+        [task resume];
+    }
+}
+
+- (UIImageView *)imageView
+{
+    if (!_imageView) _imageView = [UIImageView new];
+    return _imageView;
+}
+
+- (UIImage *) image
+{
+    return self.imageView.image;
+}
+
+- (void)setImage:(UIImage *)image
+{
+    self.scrollView.zoomScale = 1.0;
+    self.imageView.image = image;
+    self.imageView.frame = CGRectMake(0, 0, image.size.width, image.size.height);
+
+    self.scrollView.contentSize = self.image ? self.image.size : CGSizeZero;
+    [self.spinner stopAnimating];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self.scrollView addSubview:self.imageView];
+}
+
+#pragma mark - Split View COntrol
+
+- (void)awakeFromNib
+{
+    self.splitViewController.delegate = self;
+}
+
+- (BOOL)splitViewController:(UISplitViewController *)svc
+   shouldHideViewController:(UIViewController *)vc
+              inOrientation:(UIInterfaceOrientation)orientation
+
+{
+    return UIInterfaceOrientationIsPortrait(orientation);
+}
+
+- (void)splitViewController:(UISplitViewController *)svc
+     willHideViewController:(UIViewController *)aViewController
+          withBarButtonItem:(UIBarButtonItem *)barButtonItem
+       forPopoverController:(UIPopoverController *)pc
+{
+    NSLog(@"%s", __func__);
+    barButtonItem.title = aViewController.title;
+    self.navigationItem.leftBarButtonItem = barButtonItem;
+
+}
+
+- (void)splitViewController:(UISplitViewController *)svc
+     willShowViewController:(UIViewController *)aViewController
+  invalidatingBarButtonItem:(UIBarButtonItem *)barButtonItem
+{
+    NSLog(@"%s", __func__);
+    self.navigationItem.leftBarButtonItem = nil;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
