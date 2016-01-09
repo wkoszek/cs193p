93cb99d504596b65f63b45aa6886bdd48c1137a5
Wojciech A. Koszek <wojciech@koszek.com>
2016-01-08 17:14:15 -0800

    Hook up the UI

--- lesson13/PhotosCDTVC.m
+++ lesson13/PhotosCDTVC.m
@@ -8,6 +8,7 @@
 
 #import "PhotosCDTVC.h"
 #import "Photo.h"
+#import "ImageViewController.h"
 
 @implementation PhotosCDTVC
 
@@ -19,6 +20,43 @@
     cell.detailTextLabel.text = photo.subtitle;
 
     return cell;
+}
+
+#pragma mark - Navigation
+
+- (void)prepareViewController:(id)vc forSegue:(NSString *)segueIdentifer fromIndexPath:(NSIndexPath *)indexPath
+{
+    Photo *photo = [self.fetchedResultsController objectAtIndexPath:indexPath];
+
+    if ([vc isKindOfClass:[ImageViewController class]]) {
+        ImageViewController *ivc = (ImageViewController *)vc;
+        ivc.imageURL = [NSURL URLWithString:photo.imageURL];
+        ivc.title = photo.title;
+    }
+}
+
+// boilerplate
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    NSIndexPath *indexPath = nil;
+    if ([sender isKindOfClass:[UITableViewCell class]]) {
+        indexPath = [self.tableView indexPathForCell:sender];
+    }
+    [self prepareViewController:segue.destinationViewController
+                       forSegue:segue.identifier
+                  fromIndexPath:indexPath];
+}
+
+// boilerplate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    id detailvc = [self.splitViewController.viewControllers lastObject];
+    if ([detailvc isKindOfClass:[UINavigationController class]]) {
+        detailvc = [((UINavigationController *)detailvc).viewControllers firstObject];
+        [self prepareViewController:detailvc
+                           forSegue:nil
+                      fromIndexPath:indexPath];
+    }
 }
 
 @end
