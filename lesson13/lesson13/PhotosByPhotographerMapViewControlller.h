f9f107471bdb5b29f3b9b0e9fbafa705cb865ab0
Wojciech A. Koszek <wojciech@koszek.com>
2016-01-10 11:49:21 -0800

    Add methods of the controller.

--- lesson13/lesson13/PhotosByPhotographerMapViewControlller.h
+++ lesson13/lesson13/PhotosByPhotographerMapViewControlller.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Photographer.h"
 
 @interface PhotosByPhotographerMapViewControlller : UIViewController
 
--- lesson13/lesson13/PhotosByPhotographerMapViewControlller.m
+++ lesson13/lesson13/PhotosByPhotographerMapViewControlller.m
@@ -7,12 +7,50 @@
 //
 
 #import "PhotosByPhotographerMapViewControlller.h"
+#import "MapKit/MapKit.h"
+#import "Photo.h"
 
-@interface PhotosByPhotographerMapViewControlller ()
+@interface PhotosByPhotographerMapViewControlller () <MKMapViewDelegate>
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (nonatomic, strong) NSArray *photosByPhotographer;
 
 @end
 
 @implementation PhotosByPhotographerMapViewControlller
 
+- (void)updateMapViewAnnotations
+{
+
+}
+
+- (void)setMapView:(MKMapView *)mapView
+{
+    _mapView = mapView;
+    self.mapView.delegate = self;
+    [self updateMapViewAnnotations];
+}
+
+- (void)setPhotographer:(Photographer *)photographer
+{
+    _photographer = photographer;
+    self.title = photographer.name;
+    self.photosByPhotographer = nil;
+    [self updateMapViewAnnotations];
+}
+
+
+
+- (NSArray *) photosByPhotographer
+{
+
+    if (!_photosByPhotographer) {
+        NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Photo"];
+        request.predicate = [NSPredicate predicateWithFormat:@"whoTook = %@", self.photographer];
+        _photosByPhotographer = [self.photographer.managedObjectContext executeFetchRequest:request
+                                                                                      error:NULL];
+
+    }
+    return _photosByPhotographer;
+}
 
 @end
