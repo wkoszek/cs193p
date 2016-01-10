bc6c6a04286e8d7d784bcb83cddaae3b44630f75
Wojciech A. Koszek <wojciech@koszek.com>
2016-01-10 11:31:15 -0800

    Add MapViewController.

---
+++ lesson13/lesson13/PhotosByPhotographerMapViewControlller.h
@@ -0,0 +1,15 @@
+//
+//  PhotosByPhotographerMapViewControlller.h
+//  lesson13
+//
+//  Created by Wojciech Koszek (home) on 1/10/16.
+//  Copyright © 2016 Wojciech Koszek (home). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotosByPhotographerMapViewControlller : UIViewController
+
+@property (nonatomic, strong) Photographer *photographer;
+
+@end
---
+++ lesson13/lesson13/PhotosByPhotographerMapViewControlller.m
@@ -0,0 +1,18 @@
+//
+//  PhotosByPhotographerMapViewControlller.m
+//  lesson13
+//
+//  Created by Wojciech Koszek (home) on 1/10/16.
+//  Copyright © 2016 Wojciech Koszek (home). All rights reserved.
+//
+
+#import "PhotosByPhotographerMapViewControlller.h"
+
+@interface PhotosByPhotographerMapViewControlller ()
+
+@end
+
+@implementation PhotosByPhotographerMapViewControlller
+
+
+@end
