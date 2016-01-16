631bbe33890f3b09fbc5ad15295776a2ce4ea706
Wojciech A. Koszek <wojciech@koszek.com>
2016-01-16 12:09:15 -0800

    Add an add photo controller + storyboard.

---
+++ lesson13/lesson13/AddPhotoViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddPhotoViewController.h
+//  lesson13
+//
+//  Created by Wojciech Koszek (home) on 1/14/16.
+//  Copyright © 2016 Wojciech Koszek (home). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddPhotoViewController : UIViewController
+
+@end
---
+++ lesson13/lesson13/AddPhotoViewController.m
@@ -0,0 +1,37 @@
+//
+//  AddPhotoViewController.m
+//  lesson13
+//
+//  Created by Wojciech Koszek (home) on 1/14/16.
+//  Copyright © 2016 Wojciech Koszek (home). All rights reserved.
+//
+
+#import "AddPhotoViewController.h"
+
+@interface AddPhotoViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *titleTextField;
+@property (weak, nonatomic) IBOutlet UITextField *subtitleTextField;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (nonatomic, strong) UIImage *image;
+@end
+
+@implementation AddPhotoViewController
+
+- (void)setImage:(UIImage *)image
+{
+    self.imageView.image = image;
+}
+
+- (UIImage *)image
+{
+    return self.imageView.image;
+}
+
+- (IBAction)takePhoto {
+}
+- (IBAction)cancel {
+}
+- (IBAction)done {
+}
+
+@end
