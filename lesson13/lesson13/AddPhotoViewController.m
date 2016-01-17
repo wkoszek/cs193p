99f2616925c941470ad4dd676e316b82cd3840fb
Wojciech A. Koszek <wojciech@koszek.com>
2016-01-16 19:10:45 -0800

    Tag photo with the location

--- lesson13/lesson13/AddPhotoViewController.m
+++ lesson13/lesson13/AddPhotoViewController.m
@@ -10,9 +10,9 @@
 #import "Photo.h"
 #import "MapKit/MapKit.h"
 @import MobileCoreServices;
+#import "UIImage+CS193p.h"
 
-
-@interface AddPhotoViewController () <UITextFieldDelegate, UIAlertViewDelegate>
+@interface AddPhotoViewController () <UITextFieldDelegate, UIAlertViewDelegate, CLLocationManagerDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *titleTextField;
 @property (weak, nonatomic) IBOutlet UITextField *subtitleTextField;
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
@@ -21,6 +21,8 @@
 @property (strong, nonatomic) NSURL *imageURL;
 @property (strong, nonatomic) NSURL *thumbnailURL;
 @property (strong, nonatomic, readwrite) Photo *addedPhoto;
+@property (strong, nonatomic) CLLocationManager *locationManager;
+@property (nonatomic) NSInteger locationErrorCode;
 
 @end
 
@@ -45,7 +47,15 @@
 
     if (![[self class] canAddPhoto]) {
         [self fatalAlert:@"This device can't add photo"];
+    } else {
+        [self.locationManager startUpdatingLocation];
     }
+}
+
+-(void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [self.locationManager stopUpdatingLocation];
 }
 
 - (BOOL) textFieldShouldReturn:(UITextField *)textField
@@ -119,14 +129,78 @@
     [self cancel];
 }
 
+- (CLLocationManager *)locationManager
+{
+    if (!_locationManager) {
+        CLLocationManager *locationManager = [[CLLocationManager alloc] init];
+        locationManager.delegate = self;
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+        _locationManager = locationManager;
+    }
+    return _locationManager;
+}
+
+- (NSURL *)imageURL
+{
+    if (!_imageURL && self.image) {
+        NSURL *url = [self uniqueDocumentURL];
+        if (url) {
+            NSData *imageData = UIImageJPEGRepresentation(self.image, 1.0);
+            if ([imageData writeToURL:url atomically:YES]) {
+                _imageURL = url;
+            }
+        }
+    }
+    return _imageURL;
+}
+
+- (NSURL *)thumbnailURL
+{
+    NSURL *url = [self.imageURL URLByAppendingPathExtension:@"thumbnail"];
+    if (![_thumbnailURL isEqual:url]) {
+        _thumbnailURL = nil;
+        if (url) {
+            UIImage *thumbnail = [self.image imageByScalingToSize:CGSizeMake(75, 75)];
+            NSData *imageData = UIImageJPEGRepresentation(thumbnail, 0.5);
+            if ([imageData writeToURL:url atomically:YES]) {
+                _thumbnailURL = url;
+            }
+        }
+    }
+    return _thumbnailURL;
+}
+
 - (void)setImage:(UIImage *)image
 {
     self.imageView.image = image;
+
+    // when image is changed, we must delete files we've created (if any)
+    [[NSFileManager defaultManager] removeItemAtURL:_imageURL error:NULL];
+    [[NSFileManager defaultManager] removeItemAtURL:_thumbnailURL error:NULL];
+    self.imageURL = nil;
+    self.thumbnailURL = nil;
 }
 
 - (UIImage *)image
 {
     return self.imageView.image;
+}
+
+- (NSURL *)uniqueDocumentURL
+{
+    NSArray *documentDirectories = [[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask];
+    NSString *unique = [NSString stringWithFormat:@"%.0f", floor([NSDate timeIntervalSinceReferenceDate])];
+    return [[documentDirectories firstObject] URLByAppendingPathComponent:unique];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    self.location = [locations lastObject];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+    self.locationErrorCode = error.code;
 }
 
 - (IBAction)takePhoto {
